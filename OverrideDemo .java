6c1ec8356471b0bd80003fdb3163f70e3e98d7e1
Omnateeta V Unnimath <160630589+omnateeta@users.noreply.github.com>
2024-11-17 11:51:21 +0530

    Create  OverrideDemo .java

---
+++ OverrideDemo .java
@@ -0,0 +1,32 @@
+class A{
+	int i,j;
+	A(int a,int b)
+	{
+		i=a;
+		j=b;
+	}
+	void show() {
+		System.out.println("a="+i+"\tb="+j);
+	}
+}
+class B extends A
+{
+	int k;
+	B(int a,int b,int c)
+	{
+		super(a,b);
+		k=c;//overriding from class A
+	}
+	void show()
+	{
+		System.out.println("k="+k);
+	}
+}
+public class OverrideDemo {
+	public static void main(String[] args)
+	{
+	    B subob=new B(1,2,3);
+	    subob.show();
+	}
+
+}
