fa0435352868241fe178f7ff2ac702d0ea4bf4b1
Omnateeta V Unnimath <160630589+omnateeta@users.noreply.github.com>
2024-12-17 16:29:03 +0530

    Create MyPackageClass.java

---
+++ MyPackageClass.java
@@ -0,0 +1,13 @@
+package mypack;
+public class MyPackageClass {
+	
+		public void displayMessage()
+		{
+			System.out.println("Hello from MyPacakge class in myclass package!..");
+		}
+		public static int addNumbers(int a,int b)
+		{
+			return a+b;
+		}
+
+}
