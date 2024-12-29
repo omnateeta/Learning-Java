9c10f63a4ecde9ac9a4fb53c1f06c4823d4f4d43
Omnateeta V Unnimath <160630589+omnateeta@users.noreply.github.com>
2024-12-29 07:51:24 +0530

    Create TestIllegal.java

---
+++ TestIllegal.java
@@ -0,0 +1,19 @@
+public class TestIllegal {
+	static void fun() throws IllegalAccessException
+	{
+		System.out.println("Inside the function");
+		throw new IllegalAccessException("Illegal Exception");
+		
+	}
+	public static void main(String[] args)
+	{
+		try {
+			fun();
+		}
+		catch(IllegalAccessException e)
+		{
+			System.out.println(e);
+		}
+	}
+
+}
