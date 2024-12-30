9e7306ddd5eb3732a2ea5c7fdfdf8054a3782fef
Omnateeta V Unnimath <160630589+omnateeta@users.noreply.github.com>
2024-12-30 14:16:35 +0530

    Create  UserException .java

---
+++ UserException .java
@@ -0,0 +1,32 @@
+class MyException extends Exception
+{
+	private int details;
+	MyException(int d)
+	{
+		details=d;
+	}
+	public String toString(){
+		return "MyException["+details+"]";
+	}
+}
+public class UserException {
+	static void compute(int a) throws MyException
+	{
+		System.out.println("Called compute ("+a+")");
+		if(a>10)
+			throw new MyException(a);
+		System.out.println("Normal Exit");
+	}
+	public static void main(String args[])
+	{
+		try {
+			compute(1);
+			compute(11);
+		}
+		catch(MyException e)
+		{
+			System.out.println("caugh"+e);
+		}
+	}
+
+}
