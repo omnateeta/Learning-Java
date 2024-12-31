a8cc73a72591ec3df2909c9f76625bbf6e8c6ea6
Omnateeta V Unnimath <160630589+omnateeta@users.noreply.github.com>
2024-12-31 09:53:05 +0530

    Create  AutoBox.java

---
+++ AutoBox.java
@@ -0,0 +1,16 @@
+public class AutoBox {
+	public static void main(System args[])
+	{
+		Integer iob,iob2;
+		int i;
+		iob = 100;
+		System.out.println("Original value of job: "+iob);
+		++iob;
+		System.out.println("After ++iob:"+iob);
+		iob2=iob+(iob/3);
+		System.out.println("After expression ion2:"+iob2);
+		i=iob+(iob/3);
+		System.out.println("i after expression:"+i);
+	}
+
+}
