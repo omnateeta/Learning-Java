4dfe9b658ce793ac2bf89713ce4d7287addfcda7
Omnateeta V Unnimath <160630589+omnateeta@users.noreply.github.com>
2025-01-03 09:06:20 +0530

    Create CharDemo.java

---
+++ CharDemo.java
@@ -0,0 +1,15 @@
+//Demonstrate for char data type
+public class CharDemo {
+	public static void main(String[] args)
+	{
+		char ch1,ch2,ch3;
+		ch1 = 88;//code for x
+		ch2 = 'Y';
+		System.out.print("ch1 and ch2: ");
+		System.out.println(ch1+" "+ch2);
+		ch3=ch2++;
+		System.out.println("increment ch2 is"+ch2);
+	}
+
+}
+
