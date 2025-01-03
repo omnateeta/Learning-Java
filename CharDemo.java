8ba4d6babb4029ecd28bb467c2f2ea4cd56e61ae
Omnateeta V Unnimath <160630589+omnateeta@users.noreply.github.com>
2025-01-03 09:07:08 +0530

    Update CharDemo.java

--- CharDemo.java
+++ CharDemo.java
@@ -3,7 +3,7 @@ public class CharDemo {
 	public static void main(String[] args)
 	{
 		char ch1,ch2,ch3;
-		ch1 = 88;//code for x
+		ch1 = 88;//code for x from asci value
 		ch2 = 'Y';
 		System.out.print("ch1 and ch2: ");
 		System.out.println(ch1+" "+ch2);
