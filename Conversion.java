6f56602b93eb61260d65f29880dd2794bf3e9872
Omnateeta V Unnimath <160630589+omnateeta@users.noreply.github.com>
2025-01-06 11:45:59 +0530

    Create Conversion.java

---
+++ Conversion.java
@@ -0,0 +1,22 @@
+//Demonstrate casts
+public class Conversion {
+	public static void main(String args[])
+	{
+		byte b;
+		int i =257;
+		double d =323.124;
+		 System.out.println("Conversion of int to byte ");
+		 b=(byte) i;
+		 System.out.println("i and b "+i+""+b);
+		 
+		 System.out.println("Conversion of double to int ");
+		 i=(byte) d;
+		 System.out.println("d and i "+d+""+i);
+		 
+		 System.out.println("Conversion of double to byte ");
+		 b=(byte) d;
+		 System.out.println("i and b "+d+""+b);
+		  
+	}
+
+}
