375dae6433f1209e219d394bcd231715f2cf3cad
Omnateeta V Unnimath <160630589+omnateeta@users.noreply.github.com>
2025-01-08 09:19:32 +0530

    Create  CirclrResizeDemo.java

---
+++ CirclrResizeDemo.java
@@ -0,0 +1,27 @@
+//Demonstate on interface concept in java
+6.interface Resizable
+{
+	void resize(int radius);
+}
+
+class Circle implements Resizable
+{
+	int radius=25;
+	public void  resize(int Newradius)
+	{
+		radius =Newradius;
+	}
+}
+public class CirclrResizeDemo {
+	public static void main(String args[]) {
+		Circle C=new Circle();
+		System.out.println("Original value of radius in circle is "+C.radius);
+	
+		C.resize(30);
+		System.out.println("Rsized value of radius in circle is "+C.radius);
+		
+	}
+
+}
+
+
