bfec64b82712c863c31f5094c4d6da79a8c1d0a7
Omnateeta V Unnimath <160630589+omnateeta@users.noreply.github.com>
2025-01-14 14:38:20 +0530

    Create StaticDemo.java

---
+++ StaticDemo.java
@@ -0,0 +1,32 @@
+class MyClass
+{
+	int m=10;
+	static int s;
+	void xxx()
+	{
+		m=10;
+		s=20;
+	}
+	static void sss()
+	{
+		
+		s=40;
+	}
+	void display()
+	{
+		System.out.println("m= "+m+" s="+s);
+	}
+}
+public class StaticDemo {
+	public static void main(String args[])
+	{
+		MyClass mc1 = new MyClass();
+		mc1.xxx();
+		mc1.display();
+		MyClass mc2 = new MyClass();
+		mc2.sss();
+		mc2.display();
+
+	}
+
+}
