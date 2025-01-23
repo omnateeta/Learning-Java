3f85d9a874c24ad3e61bc7861bcc8a90ed77f56b
Omnateeta V Unnimath <160630589+omnateeta@users.noreply.github.com>
2025-01-23 08:17:47 +0530

    Create MethodOverloadding.java

---
+++ MethodOverloadding.java
@@ -0,0 +1,34 @@
+class Area
+{
+	double len,bred,high;
+
+	public double calcarea(double l,double b)
+	{
+		
+		return l*b;
+	}
+	public double calcarea(double r)
+	{
+		return 2*3.14*r;
+	}
+	public double calcarea(double l,double b,double h)
+	{
+		return l*b*0.5;
+	}
+
+}
+public class AreaFind {
+	public static void main(String[] args)
+	{
+		Area ob= new Area();
+		double Rectangle=ob.calcarea(5,10);
+		System.out.println("Area of rectangle= "+Rectangle);
+		
+		double Circle = ob.calcarea(5);
+		System.out.println("Area of circle= "+Circle);
+		
+		double Triangle=ob.calcarea(5,6,10);
+		System.out.println("Area of Triangle= "+Triangle);
+	}
+
+}
