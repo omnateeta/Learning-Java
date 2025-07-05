3bb218e5d075ff10398200bb9e0f55632e3240f8
Omnateeta V Unnimath <160630589+omnateeta@users.noreply.github.com>
2025-07-05 21:08:53 +0530

    Create  Add Binary.java

---
+++ Add Binary.java
@@ -0,0 +1,47 @@
+/*
+Solved
+Easy
+Topics
+premium lock icon
+Companies
+Given two binary strings a and b, return their sum as a binary string.
+
+ 
+
+Example 1:
+
+Input: a = "11", b = "1"
+Output: "100"
+Example 2:
+
+Input: a = "1010", b = "1011"
+Output: "10101"
+*/
+class Solution {
+    public String addBinary(String a, String b) {
+        int n1=a.length()-1;
+        int n2=b.length()-1;
+        int c=0, base=2;
+        StringBuilder res=new StringBuilder();
+        while(n1>=0 || n2>=0)
+        {
+            int t1=0,t2=0,sum;
+            if(n1>=0)
+             t1=a.charAt(n1--)-'0';
+            if(n2>=0)
+             t2=b.charAt(n2--)-'0';
+            sum=t1+t2+c;
+            if(sum>=base)
+            {
+                c=1;
+                sum=sum-base;
+            }
+            else
+              c=0;
+            res.append(sum);
+        }
+        if(c==1)
+         res.append(c);
+        return res.reverse().toString();
+    }
+}
